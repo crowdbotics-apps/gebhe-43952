27b84821289b32e4bb1dbca706e5fb840f1db872
Crowdbotics <team@crowdbotics.com>
2023-10-15 11:54:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gebhe-43952",
+  url: "https://gebhe-43952.botics.co",
+  api: "https://gebhe-43952.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
